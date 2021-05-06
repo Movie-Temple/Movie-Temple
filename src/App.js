bdf38858b9de5e88f4607ed80366e071966c538c
gregersundvall <gregersundvall@me.com>
2021-05-06 10:14:05 +0200

    asdf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './MTLogoPerspTrans.png';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>whaddup yall</p>
+      {/* <p>whaddup yall</p> */}
+      <img src={logo}></img>
+    
     </div>
   );
 }
