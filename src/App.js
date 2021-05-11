8a672499b01d826812e96543faa7e7a27d7ad144
Emelind <emeliejlindvall@gmail.com>
2021-05-11 08:38:48 +0200

    Test commit Emelie

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './MTLogoPerspTrans.png';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+
+// Test comment for commit
+
 function App() {
   return (
     <div className="App">
