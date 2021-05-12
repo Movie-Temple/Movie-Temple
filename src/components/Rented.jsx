77ad01c9ac87a3a3dedd91271aa94be1733dd250
Emelind <emeliejlindvall@gmail.com>
2021-05-12 08:24:48 +0200

    initial commit rented component

---
+++ src/components/Rented.jsx
@@ -0,0 +1,19 @@
+import '../rented.css'
+
+const Rented = () => {
+
+    return (
+        <div className='rented'>
+            <div className='rented-browser'>
+                RENTED COMPONENT.
+                <br/>Component for side scroll goes here instead of image placeholders
+                <img className='rented-image' src="" alt="" />
+                <img className='rented-image' src="" alt="" />
+                <img className='rented-image' src="" alt="" />
+                <img className='rented-image' src="" alt="" />
+            </div> 
+        </div>
+    )
+}
+
+export default Rented;
