469714dae7ef79a86346503c9eee4a3a3a64d996
Emelind <emeliejlindvall@gmail.com>
2021-05-12 08:20:32 +0200

    initial commit purchased component

---
+++ src/components/Purchased.jsx
@@ -0,0 +1,19 @@
+import '../purchased.css'
+
+const Purchased = () => {
+
+    return (
+        <div className='purchased'>
+            <div className='purchased-browser'>
+                PURCHASED COMPONENT.
+                <br/>Component for side scroll goes here instead of image placeholders
+                <img className='purchased-image' src="" alt="" />
+                <img className='purchased-image' src="" alt="" />
+                <img className='purchased-image' src="" alt="" />
+                <img className='purchased-image' src="" alt="" />
+            </div> 
+        </div>
+    )
+}
+
+export default Purchased;
