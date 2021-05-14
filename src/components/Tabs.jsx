826dd85e60e53c5970240e5f402d057117149fd3
gregersundvall <gregersundvall@me.com>
2021-05-14 11:21:58 +0200

    init

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,13 @@
+
+const Tabs = () => {
+
+    return (
+        <section id='tabs'>
+            <p>Categories</p>;
+            <p>Watchlist</p>;
+            <p></p>
+        </section>
+    )
+}
+
+export default Tabs;
