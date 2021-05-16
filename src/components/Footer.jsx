fc08a47215e2c375ce4f16cd0170ef8f3c80da2d
Emelind <emeliejlindvall@gmail.com>
2021-05-16 17:47:47 +0200

    initial commit footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import '../footer.css'
+
+const Footer = () => {
+
+    return (
+
+        <div className='footer'>
+            <p className='footer-element'>About us</p>
+            <p className='footer-element'>Support</p>
+            <p className='footer-element'>Career</p>
+            <p className='footer-element'>Privacy</p>
+            <p className='footer-element'>FAQ</p>
+        </div>
+    )
+}
+
+export default Footer;
