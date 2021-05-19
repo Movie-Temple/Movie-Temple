f4c81524075cba3d12b20999d9d06fb016c6e14a
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-05-19 16:01:16 +0800

    Able to display each Tab's corresponding content

--- src/components/section/Home.jsx
+++ src/components/section/Home.jsx
@@ -1,23 +1,35 @@
 import React, { Component } from 'react'
-
+import { useState } from 'react';
 
 import '../tabs/tabs.css'
 import { AppBar, Tabs, Tab } from '@material-ui/core';
+import Categories from '../Categories';
+import Purchased from '../Purchased';
+import Rented from '../Rented';
+import WatchList from '../Watchlist';
 
 const Home = () => {
-   
+        const [selectedTab, setSelectedTab] = useState(0);
+        const handleChange = (event, newValue) => {
+            setSelectedTab(newValue);
+        };
+
         return (
             <div>
                 <AppBar position="static">
 
-                    <Tabs>
-                        <Tab label="Dumpling" />
-                        <Tab label="Doggy" />
-                        <Tab label="Taipei" />
+                    <Tabs value={selectedTab} onChange={handleChange}>
+                        <Tab label="Categories" />
+                        <Tab label="Purchased" />
+                        <Tab label="Rented" />
+                        <Tab label="Watch List" />
                     </Tabs>
 
                 </AppBar>
-
+                {selectedTab === 0 && <Categories />}
+                {selectedTab === 1 && <Purchased />}
+                {selectedTab === 2 && <Rented />}
+                {selectedTab === 3 && <WatchList />}
 
             </div>
         )
