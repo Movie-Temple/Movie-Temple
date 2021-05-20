fc612ff24829ee487bc28f284a550fd384edc9b0
gregersundvall <gregersundvall@me.com>
2021-05-20 10:16:41 +0200

    done

--- src/components/SideScroller.jsx
+++ src/components/SideScroller.jsx
@@ -6,56 +6,29 @@ import getMovie from "./api";
 
 const SideScroller = ({scrollerName, titles}) => {
     const [movies, setMovies] = useState([]);
-    const [content, setContent] = useState();
+    
+
 
     useEffect(() => {
-        let moviesTemp = [];
-        
-        titles.forEach(title => {
-            async function fetchData() {
-                const movie = await getMovie(title);
-                moviesTemp.push(movie);
-            }
-            return fetchData();
-        });
-        setMovies(moviesTemp);
+        const fetchAll = async () => setMovies(
+            await Promise.all(titles.map(getMovie))
+        );
+        fetchAll();
     }, [titles])
-   
 
-    useEffect(() => {
-        setContent(() => {
-            movies.map((movie) =>{
-                return (
-                    <div>
-                        <img src={movie.Poster} alt="" />
-                        <p>{movie.Title}</p>
-                    </div>
-                );
-            })
-        })
-    })
-
-
-    // useEffect(() => {
-    //     console.log(movies)
-    //     setContent(() => {
-    //         return (
-    //             movies.map((movie) => {
-    //                 console.log(movie);
-    //                 return (
-    //                     <div className='scrollerItem'>
-    //                         <img src={movie.Poster} alt={movie.Title}  />
-    //                         <p>{movie.Title}</p>
-    //                     </div>
-    //                 );
-    //             })
-    //         );
-    //     });
-    // }, [movies])
 
     return (
         <div className='sideScroller'>
-            {content}
+            {movies.map((movie) => {
+                return (
+                    <div className='scrollerItem'>
+                        <img src={movie.Poster} alt={movie.Title} />
+                        <p>{movie.Title}</p>
+                    </div>
+                );
+                })
+
+            }   
         </div>
     )
 }
