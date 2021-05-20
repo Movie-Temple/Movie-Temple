c3d76f52252d4c58901eecb8ab3c60cbaaee6d28
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-05-20 20:20:23 +0800

    Able to Route Items in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,41 @@
-import '../footer.css'
+import '../footer.css';
+import { useHistory } from 'react-router-dom';
+import { Router, Route, Link, Switch } from 'react-router-dom';
+import AboutUs from './AboutUs';
 
 const Footer = () => {
 
-    return (
+    const history = useHistory();
+  
+    function clickAboutUs() {
+      history.push("/AboutUs");
+    }
 
-        <div className='footer'>
-            <p className='footer-element'>About us</p>
-            <p className='footer-element'>Support</p>
-            <p className='footer-element'>Career</p>
-            <p className='footer-element'>Privacy</p>
-            <p className='footer-element'>FAQ</p>
-        </div>
-    )
+    function clickSupport() {
+        history.push("/Support");
+    }
+
+    function clickCareer() {
+        history.push("/Career");
+    }
+      
+    function clickPolicy() {
+        history.push("/Policy");
+    }
+    
+    function clickFaq() {
+        history.push("/FAQ");
+    }
+
+    return (
+        <div>
+            <p className='footer-element' onClick={clickAboutUs} >About us</p>
+            <p className='footer-element' onClick={clickSupport}>Support</p>
+            <p className='footer-element' onClick={clickCareer}>Career</p>
+            <p className='footer-element' onClick={clickPolicy}>Policy</p>
+            <p className='footer-element' onClick={clickFaq}>FAQ</p>
+            </div>
+    );
 }
 
 export default Footer;
