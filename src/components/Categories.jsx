49f735234632d9ee20b336c883600da4ac9b2891
gregersundvall <gregersundvall@me.com>
2021-05-25 11:46:25 +0200

    asdf

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,24 @@
 import '../categories.css'
 import SideScroller from './SideScroller'
 import movieDb from './movieDb';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 const Categories = () => {
+    const movie = useSelector(state => state.currentMovie);
+    const movies = useSelector(state => state.movies);
+
+    function getByGenre(query) {
+        movies.filter(function(movie){return})
+    }
 
     return (
         <div className='categories'>
 
-            <SideScroller scrollerName='Comedy' titles={["meet the parents", "mean girls", "pulp fiction", "hot fuzz", "palm springs", "booksmart", "the mask"]}/>
+            <SideScroller scrollerName='Comedy' movies={}/>
             <SideScroller scrollerName='Action' titles={['black panther', 'avengers: endgame', 'mission: impossible - fallout', 'mad max: fury road', 'wonder woman', 'dunkirk', 'the dark knight']}/>
             <SideScroller scrollerName="Popular" titles={[movieDb[0], movieDb[1], movieDb[2], movieDb[3], movieDb[4], movieDb[5]]}/>
-    
+
             {/* <div className='categories-container'>
                 <h3 className='categories-header'>Category</h3>
                 <hr className='categories-breakline'/>
