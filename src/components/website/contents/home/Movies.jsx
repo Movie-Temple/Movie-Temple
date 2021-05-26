98132be183b2dae71f3b1201fedeb00948ef4f62
gregersundvall <gregersundvall@me.com>
2021-05-26 11:44:18 +0200

    asdf

--- src/components/website/contents/home/Movies.jsx
+++ src/components/website/contents/home/Movies.jsx
@@ -11,19 +11,7 @@ const Movies = () => {
 
             <SideScroller scrollerName='Comedy' genre={'comedy'}/>
             <SideScroller scrollerName='Action' genre= {'action'}/>
-            <SideScroller scrollerName="Popular" titles={[movieDb[0], movieDb[1], movieDb[2], movieDb[3], movieDb[4], movieDb[5]]}/>
-    
-            {/* <div className='categories-container'>
-                <h3 className='categories-header'>Category</h3>
-                <hr className='categories-breakline'/>
-                <div className='categories-browser'>
-                    Component for side scroll goes here instead of image placeholders
-                    <img className='categories-image' src="" alt="" />
-                    <img className='categories-image' src="" alt="" />
-                    <img className='categories-image' src="" alt="" />
-                    <img className='categories-image' src="" alt="" />
-                </div> 
-            </div> */}
+            
         </div>
     )
 
