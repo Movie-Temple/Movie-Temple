01c85068eb3f11c22f4bcf8bc010f4746167a265
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-05-27 12:28:51 +0800

    Removed test code in Search.jsx

--- src/components/website/contents/search/Search.jsx
+++ src/components/website/contents/search/Search.jsx
@@ -1,33 +1,13 @@
-import React, { Component } from 'react';
-import { db } from '../../../../firebase';
-const Search = () => {
+import React, { Component } from 'react'
 
-    const addFriestore = () => {
-        console.log("Clicked !");
-
-        db.collection("users").add({
-            first: "Ada",
-            last: "Lovelace",
-            born: 1815
-        })
-        .then((docRef) => {
-            console.log("Document written with ID: ", docRef.id);
-        })
-        .catch((error) => {
-            console.error("Error adding document: ", error);
-        });
-
-        console.log("Clicked 2!");
-    };
-    
+export class Search extends Component {
+    render() {
         return (
             <div>
                 <h2>Search Component</h2>
-                <button onClick={addFriestore}> Firestore!!! </button>
-                
             </div>
         )
-    
+    }
 }
 
-export default Search
+export default Search;
