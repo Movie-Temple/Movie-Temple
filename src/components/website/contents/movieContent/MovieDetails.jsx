c0e37f37145c0965bcae0eda73adfe3ac09ad930
Emelind <emeliejlindvall@gmail.com>
2021-05-28 14:41:20 +0200

    added basic add to watchlist and rent func.

--- src/components/website/contents/movieContent/MovieDetails.jsx
+++ src/components/website/contents/movieContent/MovieDetails.jsx
@@ -9,23 +9,60 @@ const MovieDetails = () => {
     const movie = useSelector(state => state.currentMovie);
     // CHANGE BUTTONS ACCORDING TO PROFILE. Already bought? Already rented? Already added to watchlist?
 
-    
+    // BUY MOVIE
     const buyMovie = () => {
-
-        db.collection("PURCHASED").doc(currentUserUid).set({
-            movieID: movie.Title,
-            timeStamp: ""
-        })
-        .then(() => {
-            console.log("buuuyyy Document successfully written!");
-        })
-        .catch((error) => {
-            console.error("buuuyyy Error writing document: ", error);
-        });
-        
+        if (currentUserUid) {
+            // HERE WE HAVE TO CHECK IF USER HAS ADDED PAYMENT DETAILS
+            db.collection("PURCHASED").doc(currentUserUid).set({
+                movieID: movie.Title,
+                timeStamp: ""
+            })
+            .then(() => {
+                console.log("buuuyyy Document successfully written!");
+            })
+            .catch((error) => {
+                console.error("buuuyyy Error writing document: ", error);
+            }); 
+        } else {
+            console.log('no useruid, one have to log in to buy a movie...')
+        }
     }
 
+    // RENT A MOVIE
+    const rentMovie = () => {
+        if (currentUserUid) {
+            // HERE WE HAVE TO CHECK IF USER HAS ADDED PAYMENT DETAILS
+            db.collection("RENTED").doc(currentUserUid).set({
+                movieID: movie.Title,
+                timeStamp: ""
+            })
+            .then(() => {
+                console.log("movie rented");
+            })
+            .catch((error) => {
+                console.error("error renting movie", error);
+            }); 
+        } else {
+            console.log('no useruid, one have to log in to rent a movie...')
+        }
+    }
 
+    // ADD TO WATCHLIST
+    const addToWatchlist = () => {
+        if (currentUserUid) {
+            db.collection("WATCHLIST").doc(currentUserUid).set({
+                movieID: movie.Title
+            })
+            .then(() => {
+                console.log("added to watchlist");
+            })
+            .catch((error) => {
+                console.error("error adding to watchlist ", error);
+            }); 
+        } else {
+            console.log('no useruid, one have to log in to add to watchlist...')
+        }
+    }
 
     if (movie == null) {
         return (
@@ -46,9 +83,9 @@ const MovieDetails = () => {
                     <p className='movie-details-metascore'>Metascore: {movie.Metascore}</p>
                     <p className='movie-details-genre'>Genre: {movie.Genre}</p>
                     <div className='movie-details-buttons'>
-                        <button className='rent-button'>Rent</button>
-                        <button onClick= {buyMovie} className='buy-button'>Buy</button>
-                        <button className='watchlist-button'>Add to Watchlist</button>
+                        <button onClick={rentMovie} className='rent-button'>Rent</button>
+                        <button onClick={buyMovie} className='buy-button'>Buy</button>
+                        <button onClick={addToWatchlist} className='watchlist-button'>Add to Watchlist</button>
                     </div>
                 </div>
             </div>
@@ -57,25 +94,3 @@ const MovieDetails = () => {
 }
 
 export default MovieDetails;
-
-/*  ***** For layout testing purposes ***** /Emelie
-
-            <div className='movie-details'>
-                <img className='movie-details-image' src="https://m.media-amazon.com/images/M/MV5BOGE4NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg" alt="Poster" />
-            
-                <div className='movie-details-information'>
-                    <h2 className='movie-details-title'>Thor</h2>
-                    <p className='movie-details-description'>The warrior Thor (Hemsworth) is cast out of the fantastic realm of Asgard by his father Odin (Hopkins) for his arrogance and sent to Earth to live among humans. Falling in love with scientist Jane Foster (Portman) teaches Thor much-needed lessons, and his new-found strength comes into play as a villain from his homeland sends dark forces toward Earth.</p>
-                    <p className='movie-details-actors'><span className='movie-details-subhead'>Actors</span> Chris Hemsworth, Natalie Portman, Tom Hiddleston, Anthony Hopkins</p>
-                    <p className='movie-details-runtime'><span className='movie-details-subhead'>Runtime</span> 115 min</p>
-                    <p className='movie-details-year'><span className='movie-details-subhead'>Release year</span> 2011</p>
-                    <p className='movie-details-metascore'><span className='movie-details-subhead'>Metascore</span> 57</p>
-                    <p className='movie-details-genre'><span className='movie-details-subhead'>Genre</span> Action, Adventure, Fantasy</p>
-                    <div className='movie-details-buttons'>
-                        <button className='rent-button'>Rent</button>
-                        <button className='buy-button'>Buy</button>
-                        <button className='watchlist-button'>Add to Watchlist</button>
-                    </div>
-                </div>
-            </div>
-*/
