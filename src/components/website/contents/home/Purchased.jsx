b023dd770b3195957416f7a33b69de5acca9347c
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-05-31 12:17:56 +0800

    Able to display purchased movie in Purchased

--- src/components/website/contents/home/Purchased.jsx
+++ src/components/website/contents/home/Purchased.jsx
@@ -3,32 +3,39 @@ import { useSelector } from 'react-redux';
 
 
 
+
 const Purchased = () => {
 
-    const currentUserUid = useSelector(state => state.currentUserUid);
-    
-
+    //const currentUserUid = useSelector(state => state.currentUserUid);
     const purchasedMovies = useSelector(state => state.purchasedMovies);
-
+    const movies = useSelector((state) => state.movies);
     
-
+    const lowerCase = purchasedMovies.toLowerCase;
 
     return (
         <div className='purchased'>
-            <h1>{currentUserUid}</h1>
-            <h1>{purchasedMovies}</h1>
-            <div className='purchased-browser'>
-                <img className='purchased-image' src="" alt="" />
-                <img className='purchased-image' src="" alt="" />
-                <img className='purchased-image' src="" alt="" />
-                <img className='purchased-image' src="" alt="" />
-                <img className='purchased-image' src="" alt="" />
-                <img className='purchased-image' src="" alt="" />
-                <img className='purchased-image' src="" alt="" />
-                <img className='purchased-image' src="" alt="" />
-            </div> 
+            
+            {movies.map((movie) => {
+                    return ((movie.Title.indexOf(purchasedMovies) !== -1) ?
+                        <div className='scrollerItem' key={movie.imdbID}>
+                        
+                            <img className='scrollerImg' src={movie.Poster} alt={movie.Title} />
+                            
+                            
+                        </div>
+                        : null
+                    );
+                })}  
+
+
+
+
+            
+
+            
         </div>
     )
 }
 
 export default Purchased;
+
