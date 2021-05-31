62d5c041bebbaff7d1d55bde645d519512f1e296
gregersundvall <gregersundvall@me.com>
2021-05-31 15:41:39 +0200

    scrollers for genres etc done

--- src/components/website/contents/home/Movies.jsx
+++ src/components/website/contents/home/Movies.jsx
@@ -1,21 +1,15 @@
 import './movies.css';
 import GenreScroller from './GenreScroller';
 import PersonalScroller from './PersonalScroller';
-import { useSelector } from 'react-redux';
-import { useState } from 'react';
 
 const Movies = () => {
-
-    const movies = useSelector(state => state.movies);
-    //const userID = useSelector(state => state.currentUserUid);
-    const [user, setUser] = useState({purchased: ["tt0067927", "tt0113855"], rented: ["tt5109280", "tt0017136"], watchList: ["tt0133093", "tt0092890"]});
-    
-
     
 
     return (
         <div className='categories'>
+            <PersonalScroller scrollerName='Purchased' category='purchased' />
             <PersonalScroller scrollerName='Purchased' category='rented' />
+            <PersonalScroller scrollerName='Purchased' category='watchList' />
             <GenreScroller scrollerName='Comedy' genre={'comedy'}/>
             <GenreScroller scrollerName='Action' genre= {'action'}/>
             <GenreScroller scrollerName='Drama' genre= {'drama'}/>
--- src/components/website/contents/home/PersonalScroller.jsx
+++ src/components/website/contents/home/PersonalScroller.jsx
@@ -1,76 +1,20 @@
 import './scroller.css';
 import { useHistory } from 'react-router-dom';
-
-//import { Router, Route, Link, Switch } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { replaceMovie } from "../../../../features/currentMovie";
-import { useState } from 'react';
-
-import { db } from '../../../../firebase'
 
 
 const PersonalScroller = ({scrollerName, category}) => {
-    //const uuid = useSelector(state => state.currentUserUid);
-    const uuid = 'vSwz4kNz0gPxZbhD8ugqToGLwyx1';
-    const movies = useSelector(state => state.movies);
     const history = useHistory();
     const dispatch = useDispatch();
-    const [selectedMovies, setSelectedMovies] = useState([]);
-    const [user, setUser] = useState();
+    const content = [];
 
-    
-    //console.log(movies)
-
-    db.collection("CUSTOMERS").doc(uuid)
-        .onSnapshot((doc) => {
-            let purchased = doc.data().purchased;
-            console.log(purchased); 
-
-
-            console.log(movies)
-            // Object.keys(purchased).forEach(key => 
-            //     {const match = movies.filter(movie => movie.imdbID === key);
-            //         setSelectedMovies(...selectedMovies, match);
-            //     })
-            //console.log(purchased);
-            // let ids = Object.keys(purchased);
-            // let timestamps = Object.values(purchased);
-            // console.log(ids, timestamps);
-            // for(i = 0 ; i <= ids.length ; i++ ) {
-            //     const obj = {[]}
-            // }
-            
-
-            // const selected = Object.keys(purchased).map(id =>
-            //     movies.filter(movie => movie.imdbID === id)
-            // );
-            // setSelectedMovies(selected);
-            // console.log(selectedMovies);
-            // console.log('retrieved data:', doc.data());
-    });
-    
-
-    // db.collection("CUSTOMERS").doc(uuid).collection('rented')
-    // .onSnapshot((snapshot) => {
-    //     snapshot.forEach((doc) => console.log(doc.data()))
-    
-    // });
-
-
-    const getMoviefromID = (id) => {
-        return (
-            movies.filter((movie) => movie.imdbID === id)
-        )
+    switch (category) {
+        case "rented":      {content = useSelector(state => state.rentedMovies)}
+        case "purchased":   {content = useSelector(state => state.purchasedMovies)}
+        case "watchList":   {content = useSelector(state => state.watchList)}
+        default:            {[]}
     }
-
-    // switch (category) {
-    //     case "rented": {
-    //         user.rented.forEach((id) => {
-    //             const movieToAdd = getMoviefromID(id);
-    //             setSelectedMovies(...selectedMovies + {movieToAdd})
-    //         })
-    //     }
-    // }
 
     const handleCLick = (movie) => {
         return (
@@ -82,18 +26,16 @@ const PersonalScroller = ({scrollerName, category}) => {
     return ( 
         <div className='scroller'>
             <h3 className='scrollerName'>{scrollerName}</h3>
-            
             <div className='scrollerWrapper'>
-                {selectedMovies.map((movie) => {
+                {content.map((movie) => {
                     return (
-                <div className='scrollerItem' onClick={() => {handleCLick(movie)}} key={movie.imdbID}>
+                    <div className='scrollerItem' onClick={() => {handleCLick(movie)}} key={movie.imdbID}>
                     <img className='scrollerImg' src={movie.Poster} alt={movie.Title} />
-                    {/* <p className='scrollerTitle'>{movie.Genre}</p> */}
-                </div>
+                    <p className='scrollerTitle'>{movie.Title}</p>
+                    </div>
                 );
                 })} 
             </div>
-             
         </div>
     )
 }
