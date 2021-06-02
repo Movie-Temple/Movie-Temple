23125375fb750801ddf09cf5ae01c2513e052577
gregersundvall <gregersundvall@me.com>
2021-06-02 10:26:36 +0200

    done?

--- src/components/website/contents/account/profile/personalMovieLists.js
+++ src/components/website/contents/account/profile/personalMovieLists.js
@@ -1,33 +1,27 @@
 
 
 
+import { useSelector } from 'react-redux';
 import {db} from '../../../../../firebase';
 
 
-const userID = "vSwz4kNz0gPxZbhD8ugqToGLwyx1";
+const userID = useSelector(state => state.currentUserUid);
 
 const rentMovie = ((movieID) => {
     db.collection("CUSTOMERS").doc(userID).get().then((doc) =>{
         if (doc.exists) {
-            
             const rented = doc.data().rented;
-            console.log(rented);
-
             const timestamp = Date.now();
             rented[movieID] = timestamp;
-            console.log(rented);
-
             db.collection("CUSTOMERS").doc(userID).update({
               rented
             })
             .then(() => {
-                console.log('Movie added to rented in firestore')
             })
-            
         } else {
             console.log("no such document")
         }
-    }).catch((error) => {
+        }).catch((error) => {
         console.log("error: ", error)
     });
 });
@@ -35,25 +29,19 @@ const rentMovie = ((movieID) => {
 const buyMovie = ((movieID) => {
     db.collection("CUSTOMERS").doc(userID).get().then((doc) =>{
         if (doc.exists) {
-            
             const purchased = doc.data().purchased;
-            console.log(purchased);
-
             const timestamp = Date.now();
             purchased[movieID] = timestamp;
-            console.log(purchased);
-
             db.collection("CUSTOMERS").doc(userID).update({
               purchased
             })
             .then(() => {
                 console.log('Movie added to purchased in firestore')
             })
-            
         } else {
             console.log("no such document")
         }
-    }).catch((error) => {
+        }).catch((error) => {
         console.log("error: ", error)
     });
 });
@@ -61,30 +49,79 @@ const buyMovie = ((movieID) => {
 const addToWatchlist = ((movieID) => {
     db.collection("CUSTOMERS").doc(userID).get().then((doc) =>{
         if (doc.exists) {
-            
             const watchlist = doc.data().watchlist;
-            console.log(watchlist);
-
             const timestamp = Date.now();
             watchlist[movieID] = timestamp;
-            console.log(watchlist);
-
             db.collection("CUSTOMERS").doc(userID).update({
               watchlist
             })
             .then(() => {
-                console.log('Movie added to watchlist in firestore')
             })
-            
         } else {
             console.log("no such document")
         }
-    }).catch((error) => {
+        }).catch((error) => {
         console.log("error: ", error)
     });
 });
 
 
+const getRented = () => {
+    db.collection("CUSTOMERS").doc(userID)
+        .onSnapshot((doc) => {
+            const result = doc.data().rented;
+            let rentedMoviesToAdd = [];
+            if (result) {
+                Object.keys(result).forEach(key => {
+                    const movie = movies.filter(movie => movie.imdbID === key)
+                    rentedMoviesToAdd.push(movie[0])
+                })
+                dispatch(setRentedMovies(rentedMoviesToAdd));
+            }  
+
+            //test, ska bort
+            const timestamp = Object.values(result);
+            console.log(timestamp[0]);
+            const formattedTime = Intl.DateTimeFormat('sv-SE', {dateStyle: 'full', timeStyle: 'short'}).format(timestamp[0])
+            console.log(formattedTime);
+            
+    });
+}
+
+
+const getPurchased = () => {
+    db.collection("CUSTOMERS").doc(userID)
+        .onSnapshot((doc) => {
+            const result = doc.data().purchased;
+            let moviesToAdd = [];
+            if (result) {
+                Object.keys(result).forEach(key => {
+                    const movie = movies.filter(movie => movie.imdbID === key)
+                    moviesToAdd.push(movie[0])
+                })
+                dispatch(setPurchasedMovies(moviesToAdd));
+            }
+    });
+}
+
+
+
+const getWatchlist = () => {
+    db.collection("CUSTOMERS").doc(userID)
+        .onSnapshot((doc) => {
+            const result = doc.data().watchlist;
+            let watchlistToAdd = [];
+            if (result) {
+                Object.keys(result).forEach(key => {
+                    const movie = movies.filter(movie => movie.imdbID === key)
+                    watchlistToAdd.push(movie[0])
+                })
+                dispatch(setWatchlistMovies(watchlistToAdd));
+            }
+    });
+}
+
+///remove
 //create a timestamp in unix time format:
 const timestamp = Date.now()
 //format timestamp to date and time in Sweden corrected for daylight saving changes (to show date/time on screen):
@@ -92,4 +129,4 @@ const formattedTime = Intl.DateTimeFormat('sv-SE', {dateStyle: 'full', timeStyle
 
 
 
-export {rentMovie, buyMovie, addToWatchlist};
+export {rentMovie, buyMovie, addToWatchlist, getPurchased, getRented, getWatchlist};
