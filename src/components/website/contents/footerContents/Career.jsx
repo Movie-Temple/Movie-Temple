d99f5e639174531baa7805b60af4e34625745ee2
Xizafren <56082840+Xizafren@users.noreply.github.com>
2021-06-03 16:49:09 +0200

    Bunch of css fixed and resized with media

--- src/components/website/contents/footerContents/Career.jsx
+++ src/components/website/contents/footerContents/Career.jsx
@@ -7,7 +7,7 @@ const Career = () => {
             <h2 className='career-header'>Job / Career</h2>
             <p>We are looking for fullstack developers to join our amazing and ambitious team.<br/>
             Are you the one we are looking for?<br /><br />
-            Send your resume and cover letter to: awesome_developers@teammovietemple.com
+            Send your resume and cover letter to: <br /><br />MT_developers@teammovietemple.com
             </p>
         </div>
     )
