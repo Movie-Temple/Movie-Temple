168fcb6e41fb1c282824dc16a5e2a50d315b844c
Emelind <emeliejlindvall@gmail.com>
2021-06-03 15:22:34 +0200

    items onClick to movie details OK

--- src/components/website/contents/home/Watchlist.jsx
+++ src/components/website/contents/home/Watchlist.jsx
@@ -1,15 +1,26 @@
-import { useSelector } from 'react-redux';
-import './watchlist.css'
+import './watchlist.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { replaceMovie } from "../../../../features/currentMovie";
+import { useHistory } from 'react-router';
 
 const Watchlist = () => {
 
     const watchlistMovies = useSelector(state => state.watchlistMovies);
+    const dispatch = useDispatch();
+    const history = useHistory()
+
+    const handleClick = (movie) => {
+        return (
+            dispatch(replaceMovie(movie)),
+            history.push('/moviedetails')
+        )
+    }
 
     return (
         <div className='watchlist'>
             {watchlistMovies.map((movie) => {
                     return ( watchlistMovies ?
-                        <div className='scrollerItem' key={movie.imdbID}>
+                        <div className='scrollerItem' key={movie.imdbID} onClick={() => {handleClick(movie)}}>
                             <img className='scrollerImg' src={movie.Poster} alt={movie.Title} />
                         </div>
                         : null
