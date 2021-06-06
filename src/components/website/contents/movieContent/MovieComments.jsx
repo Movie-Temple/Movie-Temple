64ea7e49d8badb20b87489368a25772d44ca55ee
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-06-06 09:19:17 +0800

    Able to upload data with UUID to FIrestore.

--- src/components/website/contents/movieContent/MovieComments.jsx
+++ src/components/website/contents/movieContent/MovieComments.jsx
@@ -1,13 +1,20 @@
 import { useSelector} from 'react-redux';
-
+import { db } from '../../../../firebase';
 import uuid from 'react-uuid'
 
 const MovieComments = () => {
     
     const movieComments = useSelector(state => state.movieComments);
+    const movie = useSelector(state => state.currentMovie);
+
     const leaveComments = () => {
+        let comments = {};
+        comments[uuid()] = "new comments";
+        db.collection('COMMENTS').doc(movie.imdbID).set({comments}, {merge: true})
         console.log("adding comments!");
     };
+
+
 
     return (
         <div className='sss'>
@@ -31,7 +38,7 @@ const MovieComments = () => {
                     );
                 })}  
             
-            
+                
                 <button onClick={leaveComments} >Leave Comments</button>
         </div>
     )
